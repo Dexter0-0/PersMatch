7fc752e2b71ba21051a79ad1abfa676012f84143
Dexter0-0 <56682055+Dexter0-0@users.noreply.github.com>
2020-07-28 20:04:05 +0300

    Update APIs.py

--- Backend/APIs/APIs.py
+++ Backend/APIs/APIs.py
@@ -4,28 +4,52 @@ import spotipy
 
 ###############################################################
 
-def GetMusicTaste(Username):
-    # Username = "c6npu7845jgtvuo2864jxzpup" (Username-ul meu pt testing daca iti trb)
-    Genres = ""
+def GetUserToken(Username, RedirectLink):
     Scope = "user-top-read"
     ClientID = 'f33ea24085634624b58c3622dedf563c'
     ClientSecret = 'af327494a245416baeb06580cf584350'
     RedirectURL = 'https://dexter0-0.github.io/'
-    Spotify = spotipy.Spotify(auth = util.prompt_for_user_token(Username, Scope, ClientID, ClientSecret, RedirectURL))
 
-    TopArtists = Spotify.current_user_top_artists()
+    File = open("RedirectLink.txt", "w+")
+    File.write(RedirectLink)
+    File.close()
 
-    for i in range(len(TopArtists)):
-        for j in range(len(TopArtists["items"][i]["genres"])):
-            Genres = Genres + TopArtists["items"][i]["genres"][j] + ","
+    Spotify = spotipy.Spotify(auth=util.prompt_for_user_token(Username, Scope, ClientID, ClientSecret, RedirectURL))
+    return Spotify
 
-    Genres = Genres.split(",")
-    return Counter(Genres)
+
+def GetMusicCompatibility(SpotifyUserOne, SpotifyUserTwo):
+    MusicScore = 0
+    GenresOne = ""
+    GenresTwo = ""
+    TopArtistsOne = SpotifyUserOne.current_user_top_artists()
+    TopArtistsTwo = SpotifyUserTwo.current_user_top_artists()
+
+    for i in range(len(TopArtistsOne)):
+        for j in range(len(TopArtistsOne["items"][i]["genres"])):
+            GenresOne = GenresOne + TopArtistsOne["items"][i]["genres"][j] + ","
+
+    for i in range(len(TopArtistsTwo)):
+        for j in range(len(TopArtistsTwo["items"][i]["genres"])):
+            GenresTwo = GenresTwo + TopArtistsTwo["items"][i]["genres"][j] + ","
+
+    GenresOne = GenresOne.split(",")
+    GenresTwo = GenresTwo.split(",")
+
+    for i in range(len(GenresOne)):
+        for j in range(len(GenresTwo)):
+            if GenresOne[i] == GenresTwo[j]:
+                MusicScore = MusicScore + 1
+
+    return MusicScore
 
 ###############################################################
 
 def main():
-    print(GetMusicTaste())
+    SpotifyUserOne = GetUserToken("Dexter", "")
+    SpotifyUserTwo = GetUserToken("Tudor", "")
+
+    print(GetMusicCompatibility(SpotifyUserOne, SpotifyUserTwo))
 
 
 if __name__ == '__main__':
