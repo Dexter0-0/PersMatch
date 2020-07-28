04be283458ec5de98606aacb62630c4f1087ac15
Dexter0-0 <56682055+Dexter0-0@users.noreply.github.com>
2020-07-28 15:27:44 +0300

    Add files via upload

---
+++ Backend/APIs/APIs.py
@@ -0,0 +1,32 @@
+from collections import Counter
+import spotipy.util as util
+import spotipy
+
+###############################################################
+
+def GetMusicTaste():
+    Genres = ""
+    Scope = "user-top-read"
+    Username = "c6npu7845jgtvuo2864jxzpup"
+    ClientID = 'f33ea24085634624b58c3622dedf563c'
+    ClientSecret = 'af327494a245416baeb06580cf584350'
+    RedirectURL = 'https://dexter0-0.github.io/'
+    Spotify = spotipy.Spotify(auth = util.prompt_for_user_token(Username, Scope, ClientID, ClientSecret, RedirectURL))
+
+    TopArtists = Spotify.current_user_top_artists()
+
+    for i in range(len(TopArtists)):
+        for j in range(len(TopArtists["items"][i]["genres"])):
+            Genres = Genres + TopArtists["items"][i]["genres"][j] + ","
+
+    Genres = Genres.split(",")
+    return Counter(Genres)
+
+###############################################################
+
+def main():
+    print(GetMusicTaste())
+
+
+if __name__ == '__main__':
+    main()
