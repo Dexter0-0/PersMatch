0c361c79bcf3a415ad2444a01fca9fc41055d3b1
Dexter0-0 <56682055+Dexter0-0@users.noreply.github.com>
2020-07-29 12:58:41 +0300

    Add files via upload

---
+++ Frontend/HomePageScripts.js
@@ -0,0 +1,12 @@
+var UsernameElement = document.getElementById("Username")
+var AgeElement = document.getElementById("Age")
+var InstagramElement = document.getElementById("Instagram")
+
+var UserUsername = "Lmao Androginescu"
+var UserAge = "69"
+var UserInstagram = "Link"
+
+UsernameElement.textContent = UserUsername;
+AgeElement.textContent = UserAge;
+InstagramElement.textContent = UserInstagram;
+
